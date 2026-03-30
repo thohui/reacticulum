c278f1d5e6a9af3342ae8d9a7321a4417e5c9fec
Tamara Klaassen <tamara@wildedame.nl>
2026-03-30 20:36:08 +0200

    revert demo page

--- playground/pages/demo.tsx
+++ playground/pages/demo.tsx
@@ -1,5 +1,4 @@
 import {
-  Align,
   Bold,
   Color,
   Divider,
@@ -18,23 +17,43 @@ export default function IndexPage() {
   const created = new Date().toDateString();
   return (
     <>
-      <Paragraph align="left" backgroundColor="#335" color="#fec">
-        <br />
-        <br />
-        <br />
-        Welcome to the Reacticulum demo page! This page showcases the components
-        available in the Reacticulum components package.
-        <br />
-        <br />
-        You can use these components to build your own Nomadnet pages.
-        <br />
-        <br />
-        This is a demo of the components package. This page was last updated on{" "}
-        {created}.
-        <br />
-        <br />
-        <br />
-      </Paragraph>
+      <H1>Header 1</H1>
+      <br />
+      <H2>Header 2</H2>
+      <br />
+      <H3>Header 3</H3>
+      <br />
+      <Paragraph>Paragraph,</Paragraph>
+      <Bold>bold</Bold>,<br />
+      <Italic>italic</Italic>,<br />
+      <Underline>underline</Underline>
+      <br />
+      Divider:
+      <br />
+      <Divider symbol="*" />
+      <br />
+      Sample input:
+      <br />
+      <Input name="input" placeholder="input" />
+      <br />
+      Sample passsword input:
+      <br />
+      <Input name="password" placeholder="password" passWord />
+      <br />
+      <Underline>
+        <Link to=":/page/index.mu">link to index</Link>
+      </Underline>
+      <br />
+      <Paragraph>Radio buttons</Paragraph>
+      <br />
+      <Radio group="options" value="option1" checked label="option1" />
+      <br />
+      <Radio group="options" value="option2" label="option2" />
+      <br />
+      Color table:
+      <Color hex="f00">█</Color>
+      <Color hex="0f0">█</Color>
+      <Color hex="00f">█</Color>
     </>
   );
 }
